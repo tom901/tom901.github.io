ec172e4f1e2ff407a8a5367a405ae5e1022d5865
Thomas Paraiso <thomas.paraiso@me.com>
2020-05-09 01:18:50 +0200

    Updates

---
+++ precache-manifest.63ddc5376fb5aaf940599a5f0e60958a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "208ca47906f05e5a7dfade3202272cd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee3de322a4e34b7626e3",
+    "url": "/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "80df7a85ebab313ef3dd",
+    "url": "/static/css/main.472fa513.chunk.css"
+  },
+  {
+    "revision": "ee3de322a4e34b7626e3",
+    "url": "/static/js/2.1c105e5c.chunk.js"
+  },
+  {
+    "revision": "6d967876016d7f4b7c601154f6d2ed4f",
+    "url": "/static/js/2.1c105e5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80df7a85ebab313ef3dd",
+    "url": "/static/js/main.2cdc851b.chunk.js"
+  },
+  {
+    "revision": "5f832a3a719b219ea531",
+    "url": "/static/js/runtime-main.cb4a4ef9.js"
+  }
+]);
